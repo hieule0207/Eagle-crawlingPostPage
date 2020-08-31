c44282c83447f94efb94546a1e7d309318e329ae
unknown <hieule0207@gmail.com>
2020-08-31 15:08:53 +0700

    for loop profile ok

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const puppeteer = require('puppeteer');
         let user = [];
         for (var i = 0; i < _user.length; i++) {
             const name = _user[i].innerText;
-            const url = _user[i].pathname;
+            const url = _user[i].getAttribute('href');
             user.push({
                 name,
                 url,
@@ -48,7 +48,76 @@ const puppeteer = require('puppeteer');
     })
     console.log(user);
     //end page
-
+    for (var i = 0; i < user.length; i++) {
+        let url = user[i].url;
+        const ProfilePage = await browser.newPage();
+        await ProfilePage.goto(url, { waitUntil: 'networkidle2' });
+        let _profile = {};
+        const name = await ProfilePage.evaluate(() => {
+            const element = document.querySelector('.gmql0nx0.l94mrbxd.p1ri9a11.lzcic4wl.bp9cbjyn.j83agx80');
+            if (element)
+                return element.innerText;
+            return '';
+        });
+        const nickname = await ProfilePage.evaluate(() => {
+            const element = document.querySelector('.m6dqt4wy.knj5qynh.h676nmdw');
+            if (element)
+                return element.innerText;
+            return '';
+        });
+        let contactAndBasicsUrl = url + '/about_contact_and_basic_info';
+        await ProfilePage.goto(contactAndBasicsUrl, { waitUntil: 'networkidle2' });
+        const gender = await ProfilePage.evaluate(() => {
+            const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi > div:nth-child(3) > div:nth-child(2)');
+            if (element)
+                return element.innerText;
+            return '';
+        });
+        const birthday = await ProfilePage.evaluate(() => {
+            const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi > div:nth-child(3) > div:nth-child(3)');
+            if (element)
+                return element.innerText;
+            return '';
+        });
+        let aboutPlacesUrl = url + '/about_places';
+        await ProfilePage.goto(aboutPlacesUrl, { waitUntil: 'networkidle2' });
+        const homeTown = await ProfilePage.evaluate(() => {
+            const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi div.oygrvhab');
+            if (element)
+                return element.innerText;
+            return '';
+        });
+        const currentTown = await ProfilePage.evaluate(() => {
+            const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi div.c9zspvje');
+            if (element)
+                return element.innerText;
+            return '';
+        });
+        let aboutWorkAndEducation = url + '/about_work_and_education';
+        await ProfilePage.goto(aboutWorkAndEducation, { waitUntil: 'networkidle2' });
+        const currentJob = await ProfilePage.evaluate(() => {
+            const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi > div:nth-child(1) div.c9zspvje');
+            if (element)
+                return element.innerText;
+            return '';
+        });
+        const Education = await ProfilePage.evaluate(() => {
+            const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi > div:nth-child(2) > div.oygrvhab');
+            if (element)
+                return element.innerText;
+            return '';
+        });
+        _profile.name = name;
+        _profile.nickname = nickname;
+        _profile.gender = gender;
+        _profile.birthday = birthday;
+        _profile.homeTown = homeTown;
+        _profile.currentTown = currentTown;
+        _profile.currentJob = currentJob;
+        _profile.Education = Education;
+        console.log(_profile);
+        await ProfilePage.close();
+    }
     //profile
     // try {
     //     let url = 'https://www.facebook.com/chrispham211'
