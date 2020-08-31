85cf8d440af7b0066a84558eefbcad3dbec98e3c
unknown <hieule0207@gmail.com>
2020-08-31 17:12:31 +0700

    look good

--- index.js
+++ index.js
@@ -49,74 +49,159 @@ const puppeteer = require('puppeteer');
     console.log(user);
     //end page
     for (var i = 0; i < user.length; i++) {
-        let url = user[i].url;
-        const ProfilePage = await browser.newPage();
-        await ProfilePage.goto(url, { waitUntil: 'networkidle2' });
-        let _profile = {};
-        const name = await ProfilePage.evaluate(() => {
-            const element = document.querySelector('.gmql0nx0.l94mrbxd.p1ri9a11.lzcic4wl.bp9cbjyn.j83agx80');
-            if (element)
-                return element.innerText;
-            return '';
-        });
-        const nickname = await ProfilePage.evaluate(() => {
-            const element = document.querySelector('.m6dqt4wy.knj5qynh.h676nmdw');
-            if (element)
-                return element.innerText;
-            return '';
-        });
-        let contactAndBasicsUrl = url + '/about_contact_and_basic_info';
-        await ProfilePage.goto(contactAndBasicsUrl, { waitUntil: 'networkidle2' });
-        const gender = await ProfilePage.evaluate(() => {
-            const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi > div:nth-child(3) > div:nth-child(2)');
-            if (element)
-                return element.innerText;
-            return '';
-        });
-        const birthday = await ProfilePage.evaluate(() => {
-            const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi > div:nth-child(3) > div:nth-child(3)');
-            if (element)
-                return element.innerText;
-            return '';
-        });
-        let aboutPlacesUrl = url + '/about_places';
-        await ProfilePage.goto(aboutPlacesUrl, { waitUntil: 'networkidle2' });
-        const homeTown = await ProfilePage.evaluate(() => {
-            const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi div.oygrvhab');
-            if (element)
-                return element.innerText;
-            return '';
-        });
-        const currentTown = await ProfilePage.evaluate(() => {
-            const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi div.c9zspvje');
-            if (element)
-                return element.innerText;
-            return '';
-        });
-        let aboutWorkAndEducation = url + '/about_work_and_education';
-        await ProfilePage.goto(aboutWorkAndEducation, { waitUntil: 'networkidle2' });
-        const currentJob = await ProfilePage.evaluate(() => {
-            const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi > div:nth-child(1) div.c9zspvje');
-            if (element)
-                return element.innerText;
-            return '';
-        });
-        const Education = await ProfilePage.evaluate(() => {
-            const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi > div:nth-child(2) > div.oygrvhab');
-            if (element)
-                return element.innerText;
-            return '';
-        });
-        _profile.name = name;
-        _profile.nickname = nickname;
-        _profile.gender = gender;
-        _profile.birthday = birthday;
-        _profile.homeTown = homeTown;
-        _profile.currentTown = currentTown;
-        _profile.currentJob = currentJob;
-        _profile.Education = Education;
-        console.log(_profile);
-        await ProfilePage.close();
+        let tempUrl = user[i].url;
+        let temp = tempUrl.indexOf("__cft__") - 1;
+        let url = tempUrl.slice(0, temp);
+        console.log(url);
+        if (url.search(/.?/) != -1) {
+            const ProfilePage = await browser.newPage();
+            await ProfilePage.goto(url, { waitUntil: 'networkidle2' });
+            let _profile = {};
+            const name = await ProfilePage.evaluate(() => {
+                const element = document.querySelector('.gmql0nx0.l94mrbxd.p1ri9a11.lzcic4wl.bp9cbjyn.j83agx80');
+                if (element)
+                    return element.innerText;
+                return '';
+            });
+            const nickname = await ProfilePage.evaluate(() => {
+                const element = document.querySelector('.m6dqt4wy.knj5qynh.h676nmdw');
+                if (element)
+                    return element.innerText;
+                return '';
+            });
+            let contactAndBasicsUrl = url + '&sk=about_contact_and_basic_info';
+            await ProfilePage.goto(contactAndBasicsUrl, { waitUntil: 'networkidle2' });
+            const gender = await ProfilePage.evaluate(() => {
+                const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi > div:nth-child(3) > div:nth-child(2)');
+                if (element)
+                    return element.innerText;
+                return '';
+            });
+            const birthday = await ProfilePage.evaluate(() => {
+                const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi > div:nth-child(3) > div:nth-child(3)');
+                if (element)
+                    return element.innerText;
+                return '';
+            });
+            let aboutPlacesUrl = url + '&sk=about_places';
+            await ProfilePage.goto(aboutPlacesUrl, { waitUntil: 'networkidle2' });
+            const homeTown = await ProfilePage.evaluate(() => {
+                const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi div.oygrvhab');
+                if (element)
+                    return element.innerText;
+                const element2 = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi > div > div:nth-child(3)');
+                if (element2)
+                    return element2.innerText;
+                return '';
+            });
+            const currentTown = await ProfilePage.evaluate(() => {
+                const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi div.c9zspvje');
+                if (element)
+                    return element.innerText;
+                const element2 = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi > div > div:nth-child(2)');
+                if (element2)
+                    return element2.innerText;
+                return '';
+            });
+            let aboutWorkAndEducation = url + '&sk=about_work_and_education';
+            await ProfilePage.goto(aboutWorkAndEducation, { waitUntil: 'networkidle2' });
+            const currentJob = await ProfilePage.evaluate(() => {
+                const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi > div:nth-child(1) div.c9zspvje');
+                if (element)
+                    return element.innerText;
+                return '';
+            });
+            const Education = await ProfilePage.evaluate(() => {
+                const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi > div:nth-child(2) > div.oygrvhab');
+                if (element)
+                    return element.innerText;
+                return '';
+            });
+            _profile.name = name;
+            _profile.nickname = nickname;
+            _profile.gender = gender;
+            _profile.birthday = birthday;
+            _profile.homeTown = homeTown;
+            _profile.currentTown = currentTown;
+            _profile.currentJob = currentJob;
+            _profile.Education = Education;
+            console.log(_profile);
+            await ProfilePage.close();
+        } else {
+            const ProfilePage = await browser.newPage();
+            await ProfilePage.goto(url, { waitUntil: 'networkidle2' });
+            let _profile = {};
+            const name = await ProfilePage.evaluate(() => {
+                const element = document.querySelector('.gmql0nx0.l94mrbxd.p1ri9a11.lzcic4wl.bp9cbjyn.j83agx80');
+                if (element)
+                    return element.innerText;
+                return '';
+            });
+            const nickname = await ProfilePage.evaluate(() => {
+                const element = document.querySelector('.m6dqt4wy.knj5qynh.h676nmdw');
+                if (element)
+                    return element.innerText;
+                return '';
+            });
+            let contactAndBasicsUrl = url + '/about_contact_and_basic_info';
+            await ProfilePage.goto(contactAndBasicsUrl, { waitUntil: 'networkidle2' });
+            const gender = await ProfilePage.evaluate(() => {
+                const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi > div:nth-child(3) > div:nth-child(2)');
+                if (element)
+                    return element.innerText;
+                return '';
+            });
+            const birthday = await ProfilePage.evaluate(() => {
+                const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi > div:nth-child(3) > div:nth-child(3)');
+                if (element)
+                    return element.innerText;
+                return '';
+            });
+            let aboutPlacesUrl = url + '/about_places';
+            await ProfilePage.goto(aboutPlacesUrl, { waitUntil: 'networkidle2' });
+            const homeTown = await ProfilePage.evaluate(() => {
+                const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi div.oygrvhab');
+                if (element)
+                    return element.innerText;
+                const element2 = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi > div > div:nth-child(3)');
+                if (element2)
+                    return element2.innerText;
+                return '';
+            });
+            const currentTown = await ProfilePage.evaluate(() => {
+                const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi div.c9zspvje');
+                if (element)
+                    return element.innerText;
+                const element2 = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi > div > div:nth-child(2)');
+                if (element2)
+                    return element2.innerText;
+                return '';
+            });
+            let aboutWorkAndEducation = url + '/about_work_and_education';
+            await ProfilePage.goto(aboutWorkAndEducation, { waitUntil: 'networkidle2' });
+            const currentJob = await ProfilePage.evaluate(() => {
+                const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi > div:nth-child(1) div.c9zspvje');
+                if (element)
+                    return element.innerText;
+                return '';
+            });
+            const Education = await ProfilePage.evaluate(() => {
+                const element = document.querySelector('div.dati1w0a.tu1s4ah4.f7vcsfb0.discj3wi > div:nth-child(2) > div.oygrvhab');
+                if (element)
+                    return element.innerText;
+                return '';
+            });
+            _profile.name = name;
+            _profile.nickname = nickname;
+            _profile.gender = gender;
+            _profile.birthday = birthday;
+            _profile.homeTown = homeTown;
+            _profile.currentTown = currentTown;
+            _profile.currentJob = currentJob;
+            _profile.Education = Education;
+            console.log(_profile);
+            await ProfilePage.close();
+        }
     }
     //profile
     // try {
